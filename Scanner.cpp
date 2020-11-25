ed928b16ca75dbe2a9a33e4a8d058d68839cca81
Tgb1801 <54073205+Tgb1801@users.noreply.github.com>
2020-11-25 12:03:35 +0530

    Added four_point_transform function

--- Scanner.cpp
+++ Scanner.cpp
@@ -65,31 +65,79 @@ bool compareContourAreas(vector<Point> contour1, vector<Point> contour2)
 	return (i < j);
 }
 
-vector<Point> findBorderPoints(vector<vector<Point>> contours){
-	int i = contours.size()-1;
+
+vector<Point> findBorderPoints(vector<vector<Point>> contours) {
+	int i = contours.size() - 1;
 	vector<Point> borderpts(4);
 
 	long summax = LONG_MIN, summin = LONG_MAX, diffmax = LONG_MIN, diffmin = LONG_MAX;
 	Point smax, smin, dmax, dmin;
 
-	for(Point j : contours[i]){
-		if(j.x + j.y >summax){
+	for (Point j : contours[i]) {
+		if (j.x + j.y > summax) {
 			summax = j.x + j.y;
 			smax = j;
-		}else if(j.x + j.y < summin){
+		}
+		else if (j.x + j.y < summin) {
 			summin = j.x + j.y;
 			smin = j;
 		}
-		if(j.y - j.x > diffmax){
+		if (j.y - j.x > diffmax) {
 			diffmax = j.y - j.x;
 			dmax = j;
-		}else if(j.y - j.x < diffmin){
+		}
+		else if (j.y - j.x < diffmin) {
 			diffmin = j.y - j.x;
 			dmin = j;
 		}
 	}
 	borderpts[0] = smin; borderpts[1] = dmin; borderpts[2] = smax; borderpts[3] = dmax;
 	return borderpts;
+}
+
+
+Mat four_point_transform(vector<vector<Point>> contours,Mat img)
+{
+	vector<Point> points = findBorderPoints(contours);
+
+	int top_lx = points[0].x,top_ly=points[0].y, top_rx = points[1].x,top_ry=points[1].y;
+
+	int bottom_rx = points[2].x, bottom_ry = points[2].y, bottom_lx = points[3].x, bottom_ly = points[3].y;
+
+	int width_top = sqrt( (top_rx-top_lx)*(top_rx-top_lx) + (top_ry-top_ly)*(top_ry-top_ly) );
+
+	int width_bottom = sqrt( (bottom_rx-bottom_lx)* (bottom_rx - bottom_lx) + (bottom_ry-bottom_ly)*(bottom_ry - bottom_ly) );
+
+	int width = max(width_top, width_bottom);
+
+	int height_left = sqrt( (top_lx-bottom_lx)*(top_lx - bottom_lx) + (top_ly-bottom_ly)*(top_ly - bottom_ly));
+
+	int height_right = sqrt( (top_rx-bottom_rx)*(top_rx-bottom_rx) + (top_ry-bottom_ry)*(top_ry-bottom_ry) );
+
+	int height = max(height_left, height_right);
+
+	Point2f src_[] = {
+					Point2f(points[0].x,points[0].y),
+					Point2f(points[1].x,points[1].y),
+					Point2f(points[2].x,points[2].y),
+					Point2f(points[3].x,points[3].y),
+	};
+
+	Point2f dst_[] = {
+	Point2f(0,0),
+	Point2f(width - 1, 0),
+	Point2f(width - 1,height - 1),
+	Point2f(0,height-1),
+	};
+
+	Mat M = getPerspectiveTransform(src_, dst_);
+
+	Mat warped;
+
+	warpPerspective(img, warped, M, Size(width, height));
+
+	return warped;
+
 }
 
 int main()
@@ -112,9 +160,9 @@ int main()
 
 	Canny(Blurred_img, edge_detected, 75, 200);	// Gives edge-detected image.
 
-	namedWindow("edge detection", WINDOW_NORMAL);
+	namedWindow("edge detected", WINDOW_NORMAL);
 
-	imshow("edge detection", edge_detected);
+	imshow("edge detected", edge_detected);
 
 	Mat Copy_Edged = edge_detected;
 
@@ -128,21 +176,30 @@ int main()
 
 	vector<Point> bpoints = findBorderPoints(contours);
 
-	Mat duplicate_image = img;
+	Mat duplicate_image = imread("ticket.jpg");
 
-	for(Point j : bpoints){
-		circle(duplicate_image, j, 10, Scalar( 0, 255, 0 ), FILLED);
+	for (Point j : bpoints) {
+		circle(duplicate_image, j, 10, Scalar(0, 255, 0), FILLED);
 	}
 
 	namedWindow("Points", WINDOW_NORMAL);
 
 	imshow("Points", duplicate_image);
 
-	drawContours(img, contours, contours.size()-1 , Scalar(0, 255, 0), 2);
+
+	Mat Copy_img = imread("ticket.jpg");	// Copy of the original image.
+
+	drawContours(Copy_img, contours, contours.size()-1 , Scalar(0, 255, 0), 2);
 
 	namedWindow("Contours", WINDOW_NORMAL);
 
-	imshow("Contours", img);
+	imshow("Contours", Copy_img);
+
+	Mat transformed = four_point_transform(contours,img);
+
+	namedWindow("Warped", WINDOW_NORMAL);
+
+	imshow("Warped", transformed);
 
 	waitKey(0);
 
